105cee36459b0ae161b94fa0f8ecf66eb5c53800
ardiana <adwiunesta@gmail.com>
2023-10-02 13:56:57 +0700

    TUGAS 3

--- fungsi.php
+++ fungsi.php
@@ -1,4 +1,15 @@
 <?php
-//membuat koneksi ke database mysql
+//membuat koneksi ke database mysql$koneksi=mysqli_connect('localhost','root','','a122106707');
 $koneksi=mysqli_connect('localhost','root','','a122106707');
+
+function encryptid($id)
+{
+    $enc = base64_encode(md5(rand().strtotime(date("H:i:s")))."-".$id);
+    return $enc;
+}
+function decryptid($id)
+{
+        $dec = explode("-",base64_decode($id));
+        return $dec[1];
+}
 ?>
--- sv_addDosen.php
+++ sv_addDosen.php
@@ -9,6 +9,20 @@ $insertNpp = $npp.$dataNpp;
 $namadosen = $_POST["namadosen"];
 $homebase = $_POST["homebase"];
 
-mysqli_query($koneksi, "INSERT INTO dosen VALUES ('$insertNpp', '$namadosen', '$homebase')");
+$q = "SELECT * FROM dosen WHERE npp='".$insertNpp."'";
+$rs = mysqli_query($koneksi,$q);
+if(mysqli_num_rows($rs) == 0)
+{
+    //simpan data
+    mysqli_query($koneksi, "INSERT INTO dosen VALUES ('$insertNpp', '$namadosen', '$homebase')");
 header("location:addDosen.php");
+}
+else
+{
+    echo "<script>
+        alert('NPP yang diinput sudah ada')
+        javascript:history.go(-1)
+        </script>";
+}
+
 ?>
